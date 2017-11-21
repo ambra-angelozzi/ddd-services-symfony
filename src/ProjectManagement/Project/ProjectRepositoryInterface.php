d15173560d0f8736217092cb60674edc58f72a11
Ambra Angelozzi <ambra.angelozzi@pay4later.com>
2017-11-21 00:08:29 +0000

    imported original classes from external repo

--- src/ProjectManagement/Project/ProjectRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BestInvestmentsLtd\ProjectManagement\Project;
-
-interface ProjectRepositoryInterface
-{
-    public function save(Project $project);
-    public function get($ref);
-}
